bc9a7a0849c373c71fa7c37e6ac78ada51ae4237
FahadQasim283 <fahadqasim3310@gmail.com>
2025-01-11 16:40:36 +0500

    add/remove from favourite done

--- script/show_details.js
+++ script/show_details.js
@@ -1,27 +1,20 @@
-function getMovieInfo() {
-  const urlParams = new URLSearchParams(window.location.search);
-    const movieData = urlParams.get("movie");
-    const movie = JSON.parse(movieData);
-    
-  if (movie) {
-    document.getElementById("movie-poster").src = movie.poster;
-    document.getElementById("movie-title").textContent = movie.title;
-    document.getElementById("movie-rating").textContent = movie.imdb_rating;
-    document.getElementById("movie-genre").textContent =
-      movie.genres.join(", ");
-    document.getElementById("movie-duration").textContent =
-      movie.duration + " min";
-    document.getElementById("movie-release-date").textContent = new Date(
-      movie.released_on
-    ).toLocaleDateString();
-    document.getElementById("movie-cast").textContent = movie.cast.join(", ");
-    document.getElementById("movie-storyline").textContent = movie.overview;
-    document.getElementById(
-      "add-fav-btn"
-    ).href = `add_to_fav.php?id=${movie.id}`;
-  } else {
-    console.log("Movie ID is missing from the URL.");
-  }
-}
+const urlParams = new URLSearchParams(window.location.search);
+const movieData = urlParams.get("movie");
+const movie = JSON.parse(movieData);
 
-getMovieInfo();
+if (movie) {
+  document.getElementById("movie-poster").src = movie.poster;
+  document.getElementById("movie-title").textContent = movie.title;
+  document.getElementById("movie-rating").textContent = movie.imdb_rating;
+  document.getElementById("movie-genre").textContent = movie.genres.join(", ");
+  document.getElementById("movie-duration").textContent =
+    movie.duration + " min";
+  document.getElementById("movie-release-date").textContent = new Date(
+    movie.released_on
+  ).toLocaleDateString();
+  document.getElementById("movie-cast").textContent = movie.cast.join(", ");
+  document.getElementById("movie-storyline").textContent = movie.overview;
+  document.getElementById("rem-fav-btn").href = `remove_fav.php?id=${movie.id}`;
+} else {
+  console.log("Movie ID is missing from the URL.");
+}
